39486d1632b673b5ac300419c64f81201259b32f
Jonas Pereira <jnsprr@outlook.com>
2020-05-28 16:58:33 -0400

    Primeiro commit do código

---
+++ estrutura-para-faca.js
@@ -0,0 +1,17 @@
+/*
+    para contador de 1 ate numero faca
+         fatorial := fatorial * contador
+    fimpara
+    escreva("O fatorial de ", numero, " é: ", fatorial)*/
+
+    function acaoBotao() {
+        var numero, fatorial, contador
+        numero = prompt("Digite o número para o calcular o fatorial: ")
+        fatorial = 1
+        for (contador = 1; contador <= numero; contador++) {
+            fatorial = fatorial * contador  
+        }
+        document.getElementById("paragrafo").innerText = "O fatorial de " + numero + " é: " + fatorial
+    }
+
+    
